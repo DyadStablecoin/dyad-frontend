6b6e8887217e27eae2d7a871de62ae2cd67b0ca5
shafu <selfouly@gmail.com>
2022-09-13 12:46:02 +0200

    sync does not work

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,16 @@
 import Button from "./Button";
-import { useState } from "react";
 import Popup from "./Popup";
 import { useDisclosure } from "@chakra-ui/react";
 import Mint from "./Mint";
+import Sync from "./Sync";
 
 export default function Row() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen: isOpenSync,
+    onOpen: onOpenSync,
+    onClose: onCloseSync,
+  } = useDisclosure();
 
   return (
     <div className="flex gap-8 border-[1px] border-white p-4 items-center">
@@ -20,7 +25,10 @@ export default function Row() {
       <Button>deposit</Button>
       <Button>withdraw</Button>
       <div className="underline underline-offset-4">5,000</div>
-      <Button>sync</Button>
+      <Button onClick={onOpenSync}>sync</Button>
+      <Popup isOpen={isOpenSync} onClose={onCloseSync}>
+        <Sync />
+      </Popup>
     </div>
   );
 }
---
+++ src/components/Sync.jsx
@@ -0,0 +1,37 @@
+import Button from "./Button";
+import abi from "../consts/abi/dyadABI.json";
+import { useState } from "react";
+import { useContractWrite, usePrepareContractWrite } from "wagmi";
+import { CONTRACT_ADDRESS } from "../consts/contract";
+
+export default function Sync() {
+  const [tokenId, setTokenId] = useState(0);
+
+  const { config } = usePrepareContractWrite({
+    addressOrName: CONTRACT_ADDRESS,
+    contractInterface: abi,
+    functionName: "syncTokenId",
+    args: [tokenId],
+  });
+
+  const { data, isLoading, isSuccess, write } = useContractWrite(config);
+
+  return (
+    <div className="flex flex-col items-center p-14 gap-8">
+      <Button
+        disabled={!write}
+        onClick={() => {
+          write();
+          console.log(333);
+          write?.();
+        }}
+      >
+        sync dNFT
+      </Button>
+      <div className="flex flex-col items-center">
+        <div>+8031 dNFTs</div>
+        <div>950,000 GAS/ 0.2 ETH</div>
+      </div>
+    </div>
+  );
+}
