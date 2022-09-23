83b0330d47ca8bff46b08e2e7128e81b764aebb1
shafu <selfouly@gmail.com>
2022-09-23 17:09:41 +0200

    rm imports

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,8 @@
 import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
-import { useForceUpdate } from "../utils/render";
 import Claim from "./Claim";
 import NFTs from "./NFTs";
 import NFTsHeader from "./NFTsHeader";
-import Row from "./Row";
-import TextInput from "./TextInput";
 
 export default function Home() {
   const { address, isConnected } = useAccount();
