13339b5ef13f347235b5db98e3cb3a5713a93aa0
shafu <selfouly@gmail.com>
2022-09-23 13:03:55 +0200

    nfts header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,15 @@ export default function Home() {
     <div className="mt-8">
       <Claim address={address} />
       {/* <NFTs address={address} /> */}
-      <div className="flex flex-col gap-2 mt-32">
+      <div className="flex flex-col gap-2 mt-24">
+        <div className="flex gap-4 mb-2">
+          <div className="ml-6 text-sm">rank</div>
+          <div className="ml-10 text-sm">value</div>
+          <div className="ml-5 text-sm">performance</div>
+          <div className="ml-2 text-sm">minted DYAD</div>
+          <div className="ml-[8rem] text-sm">invested DYAD</div>
+          <div className="ml-[22rem] text-sm">XP</div>
+        </div>
         <Row address={address} tokenId={"0"} />
         <Row address={address} tokenId={"1"} />
         <Row address={address} tokenId={"2"} />
