58c0ec173d8474c910ea8a1698cb425f0567402b
shafu <selfouly@gmail.com>
2022-09-26 20:16:31 +0200

    push state to app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { client, chains } from "./utils/wagmi-config";
+import { ChakraProvider } from "@chakra-ui/react";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiConfig } from "wagmi";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <WagmiConfig client={client}>
+        <RainbowKitProvider chains={chains}>
+          <App />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
--- src/utils/stats.js
+++ src/utils/stats.js
@@ -17,14 +17,42 @@ export function xpCurve(pL) {
   return Math.log(-pL + 1.002494) / 6 + 1;
 }
 
-export function getTVL() {
-  return 50000000;
+// get TVL
+export function useTVL(nDNFTs) {
+  nDNFTs = parseInt(nDNFTs);
+
+  let aa = [];
+  for (let i = 0; i < nDNFTs; i++) {
+    aa.push({
+      addressOrName: CONTRACT_dNFT,
+      contractInterface: abi,
+      functionName: "dyadMinted",
+      args: [i],
+    });
+  }
+
+  const { data } = useContractReads({
+    contracts: aa,
+  });
+
+  var sum = 0;
+
+  if (data) {
+    data.map((d) => {
+      if (d) {
+        sum += parseInt(d._hex);
+      }
+    });
+  }
+
+  return sum;
 }
 
 export function dNFTfloor() {
   return 15000;
 }
 
+// get average XP
 export function useAverageXP(nDNFTs) {
   nDNFTs = parseInt(nDNFTs);
 
@@ -43,7 +71,6 @@ export function useAverageXP(nDNFTs) {
   });
 
   var sum = 0;
-  console.log(data);
 
   if (data) {
     data.map((d) => {
