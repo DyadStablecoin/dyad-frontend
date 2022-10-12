66d843473fe740162e2c12a323449ba0a6b97437
shafu <selfouly@gmail.com>
2022-10-12 16:34:01 +0200

    button on hover

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,29 +1,45 @@
+import { useState } from "react";
+
 export default function Button({
   children,
   onClick,
-  isSecondary,
   isDisabled,
   bgColor, // background color
   borderColor, // background color
 }) {
+  const [isHover, setIsHover] = useState(false);
+  const handleMouseEnter = () => {
+    setIsHover(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHover(false);
+  };
+
+  function convertToBrighterColor(color) {
+    // convert #000000 to #000001
+    const colorCode = color.replace("#", "");
+    const colorCodeNumber = parseInt(colorCode, 16);
+    const colorCodeNumberBrighter = colorCodeNumber + 10000;
+    const colorCodeBrighter = colorCodeNumberBrighter.toString(16);
+    return "#" + colorCodeBrighter;
+  }
+
   return (
     <div
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       className={`border-2 pt-0 pb-0 pr-4 pl-4 cursor-pointer 
-      ${isSecondary && "border-[#FFAFAF] "}
-      ${isDisabled && !isSecondary && "opacity-50 cursor-not-allowed"}
-      ${
-        !isDisabled &&
-        !isSecondary &&
-        "hover:border-[#00ff65] hover:text-[#00ff65]"
-      }
-      ${
-        !isDisabled &&
-        isSecondary &&
-        "hover:border-[#FF5733] hover:text-[#FF5733]"
-      }
+      ${isDisabled && "opacity-50 cursor-not-allowed"}
+      ${!isDisabled && "hover:border-white"}
       `}
       style={{
-        borderColor: borderColor ? borderColor : "#4D524F",
+        borderColor: borderColor
+          ? isHover
+            ? convertToBrighterColor(borderColor)
+            : borderColor
+          : isHover
+          ? convertToBrighterColor("#4D524F")
+          : "#4D524F",
         backgroundColor: bgColor ? bgColor : "#131513",
       }}
       onClick={() => {
