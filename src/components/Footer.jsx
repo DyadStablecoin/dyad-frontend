1a765271571ed7df7c6a94242db17e8acf195c3a
shafu <selfouly@gmail.com>
2022-10-13 13:30:18 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ import { SiDiscord } from "react-icons/si";
 
 export default function Footer() {
   return (
-    <div className="p-8 flex justify-between bg-[#0f0f0f]">
-      <div className="flex gap-[3rem] ml-3">
+    <div className="flex justify-between items-center p-4">
+      <div className="flex gap-8">
         <div>Privacy Policy</div>
         <div>Disclaimer</div>
       </div>
-      <div className="flex gap-4">
+      <div className="flex gap-3">
         <TwitterOutlined />
         <MediumOutlined />
         <SiDiscord />
