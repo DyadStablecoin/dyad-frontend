0a26c2be8450ef9b5640c9bb023f24f0ee158347
shafu <selfouly@gmail.com>
2022-10-22 20:48:35 +0200

    mobile safety mode

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -43,61 +43,68 @@ export const NavBar = ({ tvl, isSafetyModeActivated }) => {
     },
   });
   return (
-    <div
-      className="flex items-center justify-between border-b border-gray-800 p-2"
-      style={{
-        backgroundColor: isSafetyModeActivated && "#2F0F13",
-      }}
-    >
-      <div className="flex gap-16 items-center justiy-center">
-        <img src={logo} alt="logo" className="w-16" />
-        <div className="hidden md:flex gap-2 items-center justify-center">
-          <WarningFilled style={{ color: "#E34158" }} />
-          <div style={{ color: "#E34158" }}>Safety Mode Activated</div>
+    <div>
+      <div
+        className="flex items-center justify-between border-b border-gray-800 p-2"
+        style={{
+          backgroundColor: isSafetyModeActivated && "#2F0F13",
+          borderBottom: "0.02rem solid #403B39",
+        }}
+      >
+        <div className="flex gap-16 items-center justiy-center">
+          <img src={logo} alt="logo" className="w-16" />
+          <div className="hidden md:flex gap-2 items-center justify-center">
+            <WarningFilled style={{ color: "#E34158" }} />
+            <div style={{ color: "#E34158" }}>Safety Mode Activated</div>
+          </div>
+        </div>
+        <div className="flex gap-8 items-center justify-center">
+          <div className="gap-8 items-center justify-center hidden md:flex  mr-8">
+            <div className="flex gap-4">
+              <div>TVL</div>
+              <div>{tvl}</div>
+            </div>
+            <div className="w-[2px] h-[2rem] bg-[#737E76]"></div>
+            <div className="flex gap-4">
+              <div>dNFT Floor</div>
+              <div>{formatUSD(dNFT_PRICE)}</div>
+            </div>
+            <div className="w-[2px] h-[2rem] bg-[#737E76]"></div>
+            <div className="flex gap-1 items-center justify-center">
+              <div>CR</div>
+              <ProgressBar percent={63} />
+            </div>
+          </div>
+          {address ? (
+            <Button
+              onClick={() => disconnect()}
+              borderColor="#463D81"
+              bgColor="#0F0D1B"
+            >
+              <div className="flex items-center gap-2">
+                <a className="cursor-pointer">Disconnect</a>
+                <div>{addressSummary(address, 3)}</div>
+              </div>
+            </Button>
+          ) : (
+            <Button
+              borderColor="#463D81"
+              bgColor="#0F0D1B"
+              onClick={() => {
+                connect({ connector: connectors[4] }); // 4 is for metamask
+              }}
+            >
+              <div className="flex gap-2 items-center justify-center">
+                <WalletOutlined />
+                <div>Connect</div>
+              </div>
+            </Button>
+          )}
         </div>
       </div>
-      <div className="flex gap-8 items-center justify-center">
-        <div className="gap-8 items-center justify-center hidden md:flex  mr-8">
-          <div className="flex gap-4">
-            <div>TVL</div>
-            <div>{tvl}</div>
-          </div>
-          <div className="w-[2px] h-[2rem] bg-[#737E76]"></div>
-          <div className="flex gap-4">
-            <div>dNFT Floor</div>
-            <div>{formatUSD(dNFT_PRICE)}</div>
-          </div>
-          <div className="w-[2px] h-[2rem] bg-[#737E76]"></div>
-          <div className="flex gap-1 items-center justify-center">
-            <div>CR</div>
-            <ProgressBar percent={63} />
-          </div>
-        </div>
-        {address ? (
-          <Button
-            onClick={() => disconnect()}
-            borderColor="#463D81"
-            bgColor="#0F0D1B"
-          >
-            <div className="flex  items-center gap-2">
-              <a className="cursor-pointer">Disconnect</a>
-              <div>{addressSummary(address, 3)}</div>
-            </div>
-          </Button>
-        ) : (
-          <Button
-            borderColor="#463D81"
-            bgColor="#0F0D1B"
-            onClick={() => {
-              connect({ connector: connectors[4] }); // 4 is for metamask
-            }}
-          >
-            <div className="flex gap-2 items-center justify-center">
-              <WalletOutlined />
-              <div>Connect</div>
-            </div>
-          </Button>
-        )}
+      <div className="md:hidden flex gap-2 items-center justify-center m-2">
+        <WarningFilled style={{ color: "#E34158" }} />
+        <div style={{ color: "#E34158" }}>Safety Mode Activated</div>
       </div>
     </div>
   );
