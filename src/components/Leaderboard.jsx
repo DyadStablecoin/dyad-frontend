2b13d97307a6a427521075fcde425466fc1562c6
shafu.eth <selfouly@gmail.com>
2022-11-13 22:38:47 +0100

    right align

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,62 +1,29 @@
-import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
+import LeaderboardRow from "./LeaderboardRow";
+
+const TABLE_HEADER = (
+  <tr className="text-[#737E76]">
+    <th></th>
+    <th>Rank</th>
+    <th>XP</th>
+    <th>value</th>
+    <th>D</th>
+    <th>W</th>
+    <th>M</th>
+    <th>Address</th>
+  </tr>
+);
 
 export default function Leaderboard() {
-  const ROW = (
-    <tr className="" style={{ border: "1px solid #3A403C" }}>
-      <td>
-        <img
-          className="w-10 h-10"
-          src="https://images.t3n.de/news/wp-content/uploads/2022/02/cryptopunk-feb-2021.jpeg?class=hero-small"
-          alt=""
-        />
-      </td>
-      <td>#8234</td>
-      <td>200,325</td>
-      <td>$50,325</td>
-      <td>
-        <div className="flex gap-2 items-center justify-center">
-          <div>
-            <ArrowUpOutlined style={{ color: "#00FF00" }} />
-          </div>
-          <div>5</div>
-        </div>
-      </td>
-      <td>
-        <div className="flex gap-2 items-center justify-center">
-          <div>
-            <ArrowDownOutlined style={{ color: "red" }} />
-          </div>
-          <div>5</div>
-        </div>
-      </td>
-      <td>
-        <div className="flex gap-2 items-center justify-center">
-          <div>
-            <ArrowUpOutlined style={{ color: "#00FF00" }} />
-          </div>
-          <div>5</div>
-        </div>
-      </td>
-      <td>monkeydluffy.eth</td>
-    </tr>
-  );
-
   return (
     <table className="leaderboard">
-      <tr>
-        <th></th>
-        <th>Rank</th>
-        <th>XP</th>
-        <th>value</th>
-        <th>D</th>
-        <th>W</th>
-        <th>M</th>
-        <th>ens</th>
-      </tr>
-      {ROW}
-      {ROW}
-      {ROW}
-      {ROW}
+      {TABLE_HEADER}
+      <LeaderboardRow />
+      <LeaderboardRow />
+      <LeaderboardRow />
+      <LeaderboardRow />
+      <LeaderboardRow />
+      <LeaderboardRow />
+      <LeaderboardRow />
     </table>
   );
 }
