736e7a389ecc507ea8231d561d2272da5556cfa7
shafu.eth <selfouly@gmail.com>
2022-12-05 01:40:56 +0100

    looks nicer

--- src/components/Loading2.jsx
+++ src/components/Loading2.jsx
@@ -6,7 +6,7 @@ export default function Loading2({ isLoading }) {
           <div role="status">
             <svg
               aria-hidden="true"
-              class="mr-2 w-[7rem] mt-3 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              class="mr-2 w-[7rem] mt-3 mb-4 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
               viewBox="0 0 100 101"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
