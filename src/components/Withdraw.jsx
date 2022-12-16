75cee796419b69e6eb548e772668f307d239e694
shafu.eth <selfouly@gmail.com>
2022-12-16 10:16:14 +0100

    max withdraw

--- src/components/Withdraw.jsx
+++ src/components/Withdraw.jsx
@@ -4,7 +4,7 @@ import abi from "../consts/abi/dNFTABI.json";
 import { useState } from "react";
 import TextInput from "./TextInput";
 import { parseEther } from "../utils/currency";
-import { round2 } from "../utils/currency";
+import { round, floor } from "../utils/currency";
 import useNft from "../hooks/useNft";
 import PopupContent from "./PopupContent";
 
@@ -21,6 +21,7 @@ export default function Withdraw({ tokenId, onClose, setTxHash }) {
 
   const { write } = useContractWrite({
     ...config,
+    args: [tokenId, parseEther(dyad - 1)],
     onSuccess: (data) => {
       onClose();
       setTxHash(data?.hash);
@@ -53,11 +54,11 @@ export default function Withdraw({ tokenId, onClose, setTxHash }) {
           </div>
           <div className="flex gap-2 items-center justify-center">
             <div className="text-[#737E76]">
-              Balance:{round2(nft.deposit / 10 ** 18)}
+              Balance:{round(nft.deposit / 10 ** 18, 2)}
             </div>
             <div
               className="text-[#584BAA] text-xl font-bold cursor-pointer"
-              onClick={() => setDyad(round2(nft.deposit / 10 ** 18))}
+              onClick={() => setDyad(floor(nft.deposit / 10 ** 18, 12))}
             >
               MAX
             </div>
