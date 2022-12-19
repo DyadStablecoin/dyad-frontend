2e155cd231cfee1f8414fe8ce8804d7c4a8f2ef9
shafu.eth <selfouly@gmail.com>
2022-12-19 23:57:29 +0100

    cache ens-name

--- src/hooks/useEnsName.js
+++ src/hooks/useEnsName.js
@@ -1,24 +1,54 @@
-import { getEnsName } from "../utils/address";
 import { useEffect, useState } from "react";
+import { useContractRead } from "wagmi";
+import { getCookie, setCookie } from "../utils/cookies";
 import { addressSummary } from "../utils/address";
+
+const RESOLVER_ABI = [
+  {
+    inputs: [
+      { internalType: "address[]", name: "addresses", type: "address[]" },
+    ],
+    name: "getNames",
+    outputs: [{ internalType: "string[]", name: "r", type: "string[]" }],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const RESOLVER_ADDRESS = "0x3671aE578E63FdF66ad4F3E12CC0c0d71Ac7510C";
 
 /**
  * Thin wrapper around the wagmi useAccount and useNetwork hook, with the only
  * difference being that we return the corresponding ENS name if it exists.
  */
 export default function useEnsName(address) {
-  const [ensName, setEnsName] = useState(address);
-  const [isLoading, setIsLoading] = useState(false);
+  const [ensName, setEnsName] = useState(addressSummary(address));
+
+  const { refetch, isLoading } = useContractRead({
+    addressOrName: RESOLVER_ADDRESS,
+    contractInterface: RESOLVER_ABI,
+    functionName: "getNames",
+    chainId: 1,
+    args: [[address]],
+    staleTime: Infinity,
+    enabled: false,
+    onSuccess: (data) => {
+      setEnsName(data[0]);
+      setCookie(`ENS_NAME_${address}`, data[0], 4);
+    },
+  });
 
   useEffect(() => {
-    async function _getEnsName() {
-      setIsLoading(true);
-      // const res = await getEnsName(address);
-      // res ? setEnsName(res) : setEnsName(addressSummary(address));
-      setEnsName(addressSummary(address));
-      setIsLoading(false);
+    const _ensName = getCookie(`ENS_NAME_${address}`);
+
+    /*
+     * Only fetch the ENS name if it's not already in the cookie.
+     */
+    if (_ensName) {
+      setEnsName(_ensName);
+    } else {
+      refetch();
     }
-    _getEnsName();
   }, [address]);
 
   return { ensName, isLoading };
---
+++ src/utils/cookies.js
@@ -0,0 +1,16 @@
+export function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+export function setCookie(name, value, days) {
+  const expires = new Date(Date.now() + days * 864e5).toUTCString();
+  document.cookie =
+    name +
+    "=" +
+    encodeURIComponent(value) +
+    "; expires=" +
+    expires +
+    "; path=/";
+}
