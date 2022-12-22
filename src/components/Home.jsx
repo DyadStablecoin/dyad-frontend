bfb227dad106949df9d6dab3137de2f917bcca16
shafu.eth <selfouly@gmail.com>
2022-12-22 23:14:18 +0100

    dropdown

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import Claim from "./Claim";
+import Dropdown from "./Dropdown";
 import NFTs from "./NFTs";
 
 export default function Home() {
   console.log("Home: Rendering");
 
+  const [option, setOption] = useState("fff");
+
   return (
     <>
-      <Claim />
-      <div className="mt-[1rem] flex justify-center items-center w-full">
-        <NFTs />
-      </div>
+      <Dropdown options={["fff", "xxx"]} onChange={setOption} />
+      <div>{option}</div>
+      {/* <Claim /> */}
+      {/* <div className="mt-[1rem] flex justify-center items-center w-full"> */}
+      {/*   <NFTs /> */}
+      {/* </div> */}
     </>
   );
 }
