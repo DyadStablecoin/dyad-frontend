2d669e6adaba393e63fcf4edd176bed165c2f197
shafu.eth <selfouly@gmail.com>
2022-12-31 16:02:04 +0100

    refactor

--- src/hooks/useCR.js
+++ src/hooks/useCR.js
@@ -4,7 +4,10 @@ import useTotalDyadSupply from "./useTotalDyadSupply";
 import { SAFETY_MODE_THRESHOLD } from "../consts/consts";
 
 // return the current collatorization ration of the protocol
-export default function useCR(newDeposit = 0) {
+//
+// `newAmountAddedToPool`: sometimes we want so simulate how CR changes when a
+// new amount is added to the pool.
+export default function useCR(newAmountAddedToPool = 0) {
   const [cr, setCR] = useState(SAFETY_MODE_THRESHOLD);
 
   const { poolBalance, refetch: refetchPoolBalance } = usePoolBalance();
@@ -24,10 +27,10 @@ export default function useCR(newDeposit = 0) {
         totalWithdrawn = 1;
       }
 
-      const _cr = ((poolBalance + newDeposit) / totalWithdrawn) * 100;
+      const _cr = ((poolBalance + newAmountAddedToPool) / totalWithdrawn) * 100;
       setCR(isNaN(_cr) ? 0 : _cr);
     }
-  }, [poolBalance, totalDyadSupply, newDeposit]);
+  }, [poolBalance, totalDyadSupply, newAmountAddedToPool]);
 
   return { cr, refetch };
 }
