a54e43544c9757e62b19180c743b42fbce23fa6e
shafu <selfouly@gmail.com>
2023-01-04 01:48:25 +0100

    eth delta

---
+++ src/hooks/useEthDelta.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { normalize } from "../utils/currency";
+import useLastEthPrice from "./useLastEthPrice";
+import useOraclePrice from "./useOraclePrice";
+
+// usd delta between the last eth price from the last sync and the current oracle price
+export default function useEthDelta() {
+  const [ethDelta, setEthDelta] = useState();
+
+  const { lastEthPrice } = useLastEthPrice();
+  const { oraclePrice } = useOraclePrice();
+
+  useEffect(() => {
+    setEthDelta(normalize(oraclePrice, 8) - lastEthPrice);
+  }, [lastEthPrice, oraclePrice]);
+
+  return { ethDelta };
+}
