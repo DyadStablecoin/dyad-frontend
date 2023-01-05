4d90dcb3da459b584edb559683471ac2e576bfc8
shafu <selfouly@gmail.com>
2023-01-05 20:51:01 +0100

    last sync caller

--- src/components/NftStatus.jsx
+++ src/components/NftStatus.jsx
@@ -1,20 +1,33 @@
+import { CheckCircleOutlined } from "@ant-design/icons";
+import { COLORS } from "../consts/colors";
 import useNftStatus, { STATUS } from "../hooks/useNftStatus";
 
 export default function NftStatus({ nft }) {
   const { status } = useNftStatus(nft);
 
   return (
-    <>
-      {status !== STATUS.RISK_FREE && (
-        <div className=" flex justify-end items-end  text-sm  ">
-          <div className="bg-[#800101] text-sm p-1 -mb-4 -mr-4 mt-9">
-            {status === STATUS.AT_LIQUIDATION_RISK && (
-              <span>Liqudation Risk</span>
-            )}
-            {status === STATUS.LIQUIDATABLE && <span>Liquidatable</span>}
+    <div className="flex justify-end items-end  text-sm">
+      {status === STATUS.SYNC_CALLER && (
+        <div
+          className={`p-1 -mb-4 -mr-4 mt-9`}
+          style={{ background: COLORS.Beige }}
+        >
+          <div className="flex gap-2 items-center justify-center">
+            <CheckCircleOutlined />
+            Synchronized
           </div>
         </div>
       )}
-    </>
+
+      {status === STATUS.AT_LIQUIDATION_RISK && (
+        <span className="bg-[#800101] p-1 -mb-4 -mr-4 mt-9">
+          Liqudation Risk
+        </span>
+      )}
+
+      {status === STATUS.LIQUIDATABLE && (
+        <span className="bg-[#800101] p-1 -mb-4 -mr-4 mt-9">Liquidatable</span>
+      )}
+    </div>
   );
 }
