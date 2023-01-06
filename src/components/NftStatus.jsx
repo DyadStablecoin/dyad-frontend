6b46ef1460822f02680ca33e37bc694db7512044
shafu <selfouly@gmail.com>
2023-01-06 20:47:06 +0100

    typo

--- src/components/NftStatus.jsx
+++ src/components/NftStatus.jsx
@@ -6,7 +6,7 @@ export default function NftStatus({ nft }) {
   const { status } = useNftStatus(nft);
 
   return (
-    <div className="flex justify-end items-end  text-sm">
+    <div className="flex justify-end items-end text-sm">
       {status === STATUS.SYNC_CALLER && (
         <div
           className={`p-1 -mb-4 -mr-4 mt-9`}
