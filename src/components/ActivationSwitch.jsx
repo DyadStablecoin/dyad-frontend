b33625b0343ec59d3b84b986ce7c39baa387351c
sdkayy <sdkay@sdkay.pw>
2023-01-28 14:41:14 -0600

    Switch fix

--- src/components/ActivationSwitch.jsx
+++ src/components/ActivationSwitch.jsx
@@ -1,5 +1,38 @@
-import { Switch } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Switch,
+  createMultiStyleConfigHelpers,
+  extendTheme,
+} from "@chakra-ui/react";
 import Label from "./Label";
+import { switchAnatomy } from "@chakra-ui/anatomy";
+
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(switchAnatomy.keys);
+
+const baseStyle = definePartsStyle({
+  // define the part you're going to style
+  container: {
+    // ...
+  },
+  thumb: {
+    bg: "rgba(255,255,255, 0.75)",
+  },
+  track: {
+    borderRadius: "1px",
+    bg: "rgba(255,255,255, 0.25)",
+    _checked: {
+      bg: "#0F0D1B",
+      border: "1px solid #463D81",
+    },
+  },
+});
+
+export const switchTheme = defineMultiStyleConfig({ baseStyle });
+
+export const theme = extendTheme({
+  components: { Switch: switchTheme },
+});
 
 export default function ActivationSwitch({
   nft,
@@ -9,12 +42,15 @@ export default function ActivationSwitch({
   return (
     <div className="flex gap-2">
       <Label>Active</Label>
-      <Switch
-        isChecked={nft.isActive}
-        onChange={() => {
-          nft.isActive ? onOpenDeactivate() : onOpenActivate();
-        }}
-      />
+      <ChakraProvider theme={theme}>
+        <Switch
+          colorScheme={"purple"}
+          isChecked={nft.isActive}
+          onChange={() => {
+            nft.isActive ? onOpenDeactivate() : onOpenActivate();
+          }}
+        />
+      </ChakraProvider>
     </div>
   );
 }
