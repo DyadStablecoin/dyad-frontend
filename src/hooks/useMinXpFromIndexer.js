18f439746e6f1ede47713bb9de9df26d1d4244f1
shafu.eth <selfouly@gmail.com>
2023-01-31 11:13:40 +0100

    fix stats

--- src/hooks/useMinXpFromIndexer.js
+++ src/hooks/useMinXpFromIndexer.js
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../utils/supabase";
+import useLastSyncVersion from "./useLastSyncVersion";
+import { CONTRACT_dNFT } from "../consts/contract";
 
 export default function useXpMinFromIndexer() {
   const [minXp, setMinXp] = useState(1);
+  const { lastSyncVersion } = useLastSyncVersion();
 
   useEffect(() => {
     supabase
       .from("nfts")
       .select("xp")
+      .eq("contractAddress", CONTRACT_dNFT)
+      .eq("version_id", lastSyncVersion)
       .order("xp", {
         ascending: true,
       })
@@ -15,7 +20,7 @@ export default function useXpMinFromIndexer() {
       .then((res) => {
         setMinXp(res.data[0].xp);
       });
-  }, []);
+  }, [lastSyncVersion]);
 
   return { minXp };
 }
