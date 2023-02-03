633de9636eaf4cb0c00a793e5b5378076079841e
Austin Quinn <sdkay@sdkay.pw>
2023-02-03 13:09:48 -0600

    Prolly forgetting some config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 const nextConfig = {
-  /* config options here */
+  swcMinify: true,
+  optimizeFonts: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
